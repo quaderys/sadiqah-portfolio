18f0f12a75af921076f5a3f5a713f8d9246ab296
Mona Lisa <monalisa@gmail.com>
2026-06-01 20:42:49 -0400

    Fix site URL to match GitHub username

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://squadery.github.io',
+  site: 'https://quaderys.github.io',
   base: '/sadiqah-portfolio',
 });
